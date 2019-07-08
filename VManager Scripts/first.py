f8218db8ae8d8beb1ddd38af79f3c6f05a5a7bbf
VictorKenichi <50708439+VictorKenichi@users.noreply.github.com>
2019-07-08 18:26:38 +0200

    Delete first.py

--- VManager Scripts/first.py
+++
@@ -1,7 +0,0 @@
-#My first script in Pyhton
-
-x=float(input("Enter the 1 st number: "))
-y=float(input("Enter the 2 nd number: "))
-z=float(input("Enter the 3 rd number: "))
-
-print("The max value is: ", max(x,y,z))
